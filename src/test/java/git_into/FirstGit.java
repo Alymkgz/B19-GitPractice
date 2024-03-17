95743677a354a3b697246cb4276498430fa77487
Alymkgz <azanbaevalym@gmail.com>
2024-03-17 09:01:29 -0700

    myBranchMessage

--- src/test/java/git_into/FirstGit.java
+++ src/test/java/git_into/FirstGit.java
@@ -4,5 +4,6 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("my first git practice Alym");
         System.out.println("mu second push Alym");
+        System.out.println("my branches push Alym branch");
     }
 }
