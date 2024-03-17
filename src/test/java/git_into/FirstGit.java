342e077aed6f102ff0cbe2d22f1810f47c6d317a
Alymkgz <azanbaevalym@gmail.com>
2024-03-17 09:33:56 -0700

    for loop added

--- src/test/java/git_into/FirstGit.java
+++ src/test/java/git_into/FirstGit.java
@@ -5,5 +5,10 @@ public class FirstGit {
         System.out.println("my first git practice Alym");
         System.out.println("mu second push Alym");
         System.out.println("my branches push Alym branch");
+
+        for (int i = 0; i < 5;i++){
+            System.out.println(i);
+            System.out.println("added another line of code branch Alym");
+        }
     }
 }
