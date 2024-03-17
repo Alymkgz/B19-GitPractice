684209e59ad518606c07ee64017bdad59926f4a5
Alymkgz <azanbaevalym@gmail.com>
2024-03-17 08:50:29 -0700

    my second push

--- src/test/java/git_into/FirstGit.java
+++ src/test/java/git_into/FirstGit.java
@@ -3,5 +3,6 @@ package git_into;
 public class FirstGit {
     public static void main(String[] args) {
         System.out.println("my first git practice Alym");
+        System.out.println("mu second push Alym");
     }
 }
